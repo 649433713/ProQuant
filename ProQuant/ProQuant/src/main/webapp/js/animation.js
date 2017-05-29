8910ef969a07fda23c764ab3d67994f42b618c12
151250164 <151250164@smail.nju.edu.cn>
2017-05-29 22:52:17 +0800

    homePage修正

--- ProQuant/ProQuant/src/main/webapp/js/animation.js
+++ ProQuant/ProQuant/src/main/webapp/js/animation.js
@@ -1,167 +1,86 @@
-/**
- * Created by LENOVO on 2017/5/24.
- */
-
-/* ---- SETTINGS ---- */
-
-var groundSize = 200,
-    nbrOfFlower = 40,
-    vel = 9,
-    colors = ["#e9c46a","#f4a261","#e76f51"]
-;
-
-windowWidth = window.innerWidth,
-    windowHeight = window.innerHeight,
-
-    mouseX = windowWidth/2,
-    mouseY = windowHeight/2,
-
-    canvas = document.createElement('canvas'),
-    context = canvas.getContext("2d"),
-
-    dragMode = false,
-    ground = null
-flowers = [];
-
-/* ---- Ground ---- */
-function Ground () {
-    this.x = windowWidth/2;
-    this.y = windowHeight/2;
-    this.size = groundSize/2
-    this.color = "#2A9D8F";
-}
-Ground.prototype.render = function() {
-    context.beginPath();
-    context.strokeStyle = this.color;
-    context.lineWidth = 2;
-    context.arc(this.x,this.y, this.size/2, 0, Math.PI*2);
-    context.stroke();
-};
-Ground.prototype.update = function(x, y){
-    this.x = x;
-    this.y = y
-}
-Ground.prototype.isInside = function(x, y){
-    var inside = false;
-    if(Math.abs(this.x - x) < this.size && Math.abs(this.y - y) < this.size){
-        inside = true;
-    }
-    return inside;
-}
-
-/* ---- Flower ---- */
-function Flower () {
-    this.color = "#2A9D8F";
-    this.flowerColor = colors[Math.randomFromInterval(0,colors.length)]
-    this.ground = {
-        x : windowWidth/2,
-        y : windowHeight/2,
-        r : groundSize/4
-    }
-    this.ampl = Math.randomFromInterval(3,8);
-    this.nbrOfStemPoint = Math.randomFromInterval(4,8);
-    this.angle = Math.radians(Math.randomFromInterval(0,360));
-    this.points = [];
-    this.stem = [];
-    for (var i = 0; i < this.nbrOfStemPoint ; i++) {
-        this.points.push({
-            x : this.ground.x + this.ground.r*Math.cos(this.angle),
-            y : this.ground.y + this.ground.r*Math.sin(this.angle)
-        });
-        this.stem.push({
-            x : this.ground.x + this.ground.r*Math.cos(this.angle),
-            y : this.ground.y + this.ground.r*Math.sin(this.angle)
-        });
-    }
-}
-Flower.prototype.render = function() {
-    context.beginPath();
-    context.strokeStyle = this.color;
-    context.lineWidth = 2;
-    context.moveTo(this.points[0],this.points[0]);
-    for (var i = 0; i < this.nbrOfStemPoint ; i++) {
-        // --
-        var destX = this.ground.x + (this.ground.r+(this.ampl*i))*Math.cos(this.angle);
-        var destY = this.ground.y + (this.ground.r+(this.ampl*i))*Math.sin(this.angle);
-
-        // --
-        this.stem[i].x += ( destX - this.stem[i].x )*(1-(i/vel));
-        this.stem[i].y += ( destY - this.stem[i].y )*(1-(i/vel));
-
-        // -- Draw
-        context.lineTo(this.stem[i].x, this.stem[i].y);
-
-        // -- Save
-        this.points[i].x = destX;
-        this.points[i].y = destY;
-    }
-    context.stroke();
-    context.beginPath();
-    context.strokeStyle = this.flowerColor;
-    context.fillStyle = "#264653";
-    context.arc(this.stem[this.nbrOfStemPoint-1].x, this.stem[this.nbrOfStemPoint-1].y, 8, 0, Math.PI*2);
-    context.stroke();
-    context.fill();
-};
-Flower.prototype.update = function(x,y){
-    this.ground.x = x;
-    this.ground.y = y;
-}
-
-/* ---- Functions ----*/
-function loop(){
-    context.clearRect(0,0, windowWidth, windowHeight);
-    ground.update(mouseX, mouseY);
-    ground.render();
-    _.chain(flowers).each(function(f, index){
-        f.update(mouseX,mouseY);
-        f.render();
-    });
-    requestAnimationFrame(loop);
-}
-
-function recordMousePosition (e) {
-    if(dragMode){
-        mouseX = e.x;
-        mouseY = e.y;
-    }
-}
-function enableDrag(e) {
-    if(ground.isInside(e.x, e.y)){
-        canvas.style.cursor = "pointer";
-        dragMode = true;
-    }
-}
-function disableDrag() {
-    dragMode = false;
-    canvas.style.cursor = "auto";
-}
-
-// Converts from degrees to radians.
-Math.radians = function(degrees) {
-    return degrees * Math.PI / 180;
-};
-Math.randomFromInterval = function(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-/* ---- START ---- */
-
-canvas.id = "canvas";
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-document.body.appendChild(canvas);
-
-canvas.addEventListener('mousemove', recordMousePosition);
-canvas.addEventListener('touchmove', recordMousePosition);
-canvas.addEventListener('mousedown', enableDrag);
-canvas.addEventListener('touchstart', enableDrag);
-canvas.addEventListener('touchend', disableDrag);
-canvas.addEventListener('mouseup', disableDrag);
-
-ground = new Ground();
-for (var i = 0; i < nbrOfFlower; i++) {
-    flowers.push(new Flower());
-}
-loop();
+// var PI_BY_180 = Math.PI / 180,
+//     PI2 = 2 * Math.PI,
+//     c = document.querySelector('#c'),
+//     ctx = c.getContext('2d'),
+//     dots = [],
+//     W = window.innerWidth,
+//     H = window.innerHeight,
+//     radius = 20;
+//
+// c.width = W;
+// c.height = H;
+// var n = (W+200) / 80;
+//
+// function Dot (cx, cy, radius, angle, size, i, j) {
+//     this.cx = cx;
+//     this.cy = cy;
+//     this.size = size;
+//     this.radius = radius;
+//     this.angle = angle;
+//     this.i = i;
+//     this.j = j;
+// }
+//
+// Dot.prototype.draw = function () {
+//     // ctx.beginPath();
+//     ctx.arc(this.x, this.y, this.size, 0, PI2, false);
+//     // ctx.fill();
+//     ctx.closePath();
+//     return this;
+// }
+//
+// Dot.prototype.update = function () {
+//     this.angle += 1.5;
+//     this.x = this.cx + this.radius * Math.cos(this.angle * PI_BY_180);
+//     this.y = this.cy + this.radius * Math.sin(this.angle * PI_BY_180);
+//     return this;
+// }
+//
+// function animate() {
+//     ctx.clearRect (0, 0, W, H);
+//     ctx.beginPath();
+//     ctx.strokeStyle = "rgba(255,255,255,0.2)";
+//     for (var i = 0; i < dots.length; i++) {
+//         for (var j = i; j < dots.length; j++) {
+//             drawLine(dots[i], dots[j]);
+//         }
+//     }
+//     ctx.stroke();
+//
+//     ctx.fillStyle = 'rgba(255,255,255,0.2)';
+//     // ctx.beginPath();
+//     for (var i = 0; i < dots.length; i++) {
+//         dots[i].update();
+//         dots[i].draw();
+//     }
+//     // ctx.fill();
+//     // ctx.closePath();
+//
+//
+//     window.requestAnimationFrame ? requestAnimationFrame(animate) : mozRequestAnimationFrame(animate);
+// }
+//
+// function random(a, b) { return a + Math.random() * (b-a); }
+//
+// function drawLine (d1, d2) {
+//     if (Math.abs(d1.i - d2.i) >= 2 || Math.abs(d1.j - d2.j) >= 2) return;
+//     ctx.moveTo(d1.x, d1.y);
+//     ctx.lineTo(d2.x, d2.y);
+// }
+//
+// for (var i = 0; i < n; i++) {
+//     var y = 20;
+//     for (var j = 0; j < n * 2.5; j++) {
+//         var cx = -100 + i * 80,
+//             cy = y;
+//         y += j;
+//
+//         // draw the dots
+//         if (j > n/2) {
+//             var d = new Dot(cx + random(-30,30), cy + random(-10,10), radius, i * 20 + j * 20, 3 * (j/n), i, j);
+//             dots.push(d);
+//         }
+//     }
+// }
+//
+// animate();
--- ProQuant/ProQuant/src/main/webapp/js/stockInfo.js
+++ ProQuant/ProQuant/src/main/webapp/js/stockInfo.js
@@ -2,5 +2,24 @@
  * Created by xiaoJun on 2017/5/16.
  */
 
+// import * as temp from '../charts/meterChart';
+
 $(function () {
+    $("#kLine div ul li").click(function () {
+        $(this).addClass("blueBackground");
+        $(this).siblings().removeClass("blueBackground");
+    });
+    var myChart = echarts.init(document.getElementById("meter"));
+    meterChart(myChart);
+    var mycharts = echarts.init(document.getElementById("main"));
+    kLine(mycharts);
+    $("#time").click(function () {
+        meterChart(mycharts);
+    });
+    $("#dayLine").click(function () {
+        kLine(mycharts);
+    });
+    $("#weekLine").click(function () {
+
+    });
 })
