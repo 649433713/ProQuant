175003b947cb35c67e6791900d670a8191940d57
151250179 <151250179@smail.nju.edu.cn>
2017-05-30 10:36:01 +0800

    有的方法实现不了，讨论完再实现吧

---
+++ ProQuant/ProQuant/src/main/java/bl/helper/StockPoToVo.java
@@ -0,0 +1,13 @@
+package bl.helper;
+
+import PO.StockData;
+import VO.StockDataVO;
+
+public class StockPoToVo{
+	
+     public static StockDataVO stockDataToStockDataVO(StockData stockData){
+//    	 StockDataVO stockDataVO=new StockDataVO(stockData.get, stockData.getId(), , highValue, lowValue, openValue, closeValue, turnoverRate, marketRate, priceEarningsRatio)
+		return null;
+    	 
+     }
+}
