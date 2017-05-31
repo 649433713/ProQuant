085b6e5827c33e3b8dbb6156e21d3aeca84fbfaf
151250164 <151250164@smail.nju.edu.cn>
2017-06-01 01:17:57 +0800

    登录框修改

--- ProQuant/ProQuant/src/main/webapp/js/header.js
+++ ProQuant/ProQuant/src/main/webapp/js/header.js
@@ -3,6 +3,7 @@
  */
 
 $(function () {
+    //设置搜索框未获得焦点时长度缩小，获得焦点时长度变大
     $("#searchInput").blur(function (){
         if($(this).val() == "" || (this).val() == null){
             $(this).attr("placeholder","搜索");
@@ -13,51 +14,15 @@ $(function () {
         $(this).attr("placeholder","请输入股票代码");
     });
 
+    //为导航栏的点击添加监听
     $("#navigation li").click(function () {
         $(this).addClass("active");
         $(this).siblings().removeClass("active");
     });
 
+    // document.querySelector('.cont_centrar').className = "cont_centrar cent_active";
 
-    var $loginMsg = $('.loginMsg'),
-        $login = $('.login'),
-        $signupMsg = $('.signupMsg'),
-        $signup = $('.signup'),
-        $frontbox = $('.frontbox');
-
-    $('#switch1').on('click', function() {
-
-        $loginMsg.toggleClass("visibility");
-        $frontbox.addClass("moving");
-        $signupMsg.toggleClass("visibility");
-
-        $signup.toggleClass('hide');
-        $login.toggleClass('hide');
-        $(this).attr("disabled",true);
-        $("#switch2").attr("disabled",false);
-
-    })
-
-    $('#switch2').on('click', function() {
-
-        $loginMsg.toggleClass("visibility");
-        $frontbox.removeClass("moving");
-        $signupMsg.toggleClass("visibility");
-
-        $signup.toggleClass('hide');
-        $login.toggleClass('hide');
-        $(this).attr("disabled",true);
-        $("#switch1").attr("disabled",false);
-    })
-
-    setTimeout(function(){
-        $('#switch1').click()
-    },1000)
-
-    setTimeout(function(){
-        $('#switch2').click()
-    },3000)
-
+    //点击登陆按钮，显示登陆弹出框
     $("#loginButton").click(function () {
         $("#loginModal").modal("show");
     })
@@ -87,6 +52,103 @@ function slidein(index, remindness) {
     window.location.href = '#toaster';
     setTimeout("window.location.href='#toaster_close'", 1500);
 }
+
+function sign_up(){
+    var inputs = document.querySelectorAll('.input_form_sign');
+    document.querySelectorAll('.ul_tabs > li')[0].className="";
+    document.querySelectorAll('.ul_tabs > li')[1].className="active";
+
+    for(var i = 0; i < inputs.length ; i++  ) {
+        if(i == 2  ){
+
+        }else{
+            document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
+        }
+    }
+
+    setTimeout( function(){
+        for(var d = 0; d < inputs.length ; d++  ) {
+            document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block active_inp";
+        }
+    },100 );
+
+    document.querySelector('.link_forgot_pass').style.opacity = "0";
+    document.querySelector('.link_forgot_pass').style.top = "-5px";
+    document.querySelector('.btn_sign').innerHTML = "SIGN UP";
+    setTimeout(function(){
+        document.querySelector('.terms_and_cons').style.opacity = "1";
+        document.querySelector('.terms_and_cons').style.top = "5px";
+    },500);
+    setTimeout(function(){
+        document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_none";
+        document.querySelector('.terms_and_cons').className = "terms_and_cons d_block";
+    },200);
+
+}
+
+
+
+function sign_in(){
+    var inputs = document.querySelectorAll('.input_form_sign');
+    document.querySelectorAll('.ul_tabs > li')[0].className = "active";
+    document.querySelectorAll('.ul_tabs > li')[1].className = "";
+
+    for(var i = 0; i < inputs.length ; i++  ) {
+        switch(i) {
+            case 1:
+                console.log(inputs[i].name);
+                break;
+            case 2:
+                console.log(inputs[i].name);
+            default:
+                document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
+        }
+    }
+
+    setTimeout( function(){
+        for(var d = 0; d < inputs.length ; d++  ) {
+            switch(d) {
+                case 1:
+                    break;
+                case 2:
+
+                default:
+                    document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block";
+                    document.querySelectorAll('.input_form_sign')[2].className = "input_form_sign d_block active_inp";
+            }
+        }
+    },100 );
+
+    document.querySelector('.terms_and_cons').style.opacity = "0";
+    document.querySelector('.terms_and_cons').style.top = "-5px";
+
+    setTimeout(function(){
+        document.querySelector('.terms_and_cons').className = "terms_and_cons d_none";
+        document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_block";
+
+    },300);
+
+    setTimeout(function(){
+
+        document.querySelector('.link_forgot_pass').style.opacity = "1";
+        document.querySelector('.link_forgot_pass').style.top = "5px";
+        for(var d = 0; d < inputs.length ; d++  ) {
+            switch(d) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign";
+            }
+        }
+    },700);
+    document.querySelector('.btn_sign').innerHTML = "SIGN IN";
+}
+
+
+
+
 
 
 
