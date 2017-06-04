3c1c14547205cd3d4092b41e4d57bdea5dc0fc39
151250164 <151250164@smail.nju.edu.cn>
2017-06-05 00:51:47 +0800

    个股

--- ProQuant/ProQuant/src/main/webapp/js/stockInfo.js
+++ ProQuant/ProQuant/src/main/webapp/js/stockInfo.js
@@ -3,6 +3,7 @@
  */
 
 // import * as temp from '../charts/meterChart';
+var buy_dialogIsShowed = false;
 
 $(function () {
     $("#kLine div ul li").click(function () {
@@ -23,12 +24,38 @@ $(function () {
 
     });
 
-
+    //点击买入按钮，显示买入提示框
     $("#buyButton").click(function (e) {
         //edit_dialogIsShowed = true;
-        e.stopPropagation();
-        $(".buy-page-holder").removeClass("disappear");
-        showPromptDoalog($(this));
+        if(buy_dialogIsShowed){
+
+        }else{
+            e.stopPropagation();
+            $(".buy-page-holder").removeClass("disappear");
+            showPromptDoalog($(this));
+            buy_dialogIsShowed = true;
+        }
     });
 
+    //当浏览器大小改变时，调整买入提示框的位置
+    window.onresize = function () {
+        if(!$(".buy-page-holder").hasClass("disappear")){
+            // alert($("#buyButton").offset().left);
+            showPromptDoalog($("#buyButton"));
+        }
+    }
+
+    //点击除买入提示框以外的提示框时，关闭提示框
+    $(document).click(function (e) {
+        // e.stopPropagation();
+        e = window.event || e;
+        var target = e.srcElement || e.target;
+        if(buy_dialogIsShowed && (!$(target).is(".buy-page-holder, .buy-page-holder *"))){
+            $(".buy-page-holder").addClass("disappear");
+            buy_dialogIsShowed = false;
+        }
+    });
+
+
 })
+
