3ea4021c4277fae7869967b0a83fdb6ec2616fb9
151250179 <151250179@smail.nju.edu.cn>
2017-06-06 14:35:34 +0800

    实现

--- ProQuant/ProQuant/src/main/java/bl/helper/StockPoToVo.java
+++ ProQuant/ProQuant/src/main/java/bl/helper/StockPoToVo.java
@@ -1,9 +1,13 @@
 package bl.helper;
 
+import PO.InfoData;
+import PO.StockCurrentData;
 import PO.StockData;
+import PO.StockScore;
 import PO.kLinePO.KLineDayData;
 import VO.StockDataVO;
 import VO.StockKLine;
+import VO.StockVO;
 
 public class StockPoToVo{
 	
@@ -18,5 +22,21 @@ public class StockPoToVo{
     	 StockKLine kLine=new StockKLine(null, 0, kLineDayData.getDate(), kLineDayData.getKline().getHigh(), kLineDayData.getKline().getLow()
     			 , kLineDayData.getKline().getOpen(),kLineDayData.getKline().getClose());
     	 return kLine;
+     }
+     
+     public static StockVO stockCurrentToStockVO(StockCurrentData stockCurrentData
+    		 ,InfoData infoData,StockScore stockScore,StockData stockData){
+    	 double amount_Of_Increase=stockCurrentData.getTrade()-stockCurrentData.getOpen();
+    	 int vo=stockCurrentData.getVolume().intValue();
+    	 StockVO sVo=new StockVO(stockCurrentData.getName(), Integer.parseInt(stockCurrentData.getCode()),
+    			 stockCurrentData.getTrade(), stockCurrentData.getHigh(), stockCurrentData.getLow(),
+    			 stockCurrentData.getOpen(), stockData.getClose(),amount_Of_Increase,
+    			 amount_Of_Increase/stockCurrentData.getTrade()
+    			 , vo, stockCurrentData.getTurnoverratio(), 
+    			 0.0, 0.0, 0.0
+    			 , stockCurrentData.getChangepercent(),
+    			 0.0, 0.0, 
+    			 infoData.getCpInfo(), infoData.getCpBusiness(), stockScore.getAnalyzeNum(), stockScore.getBeatNum())
+   return sVo;
      }
 }
