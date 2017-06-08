a03f8e795d9e29f29b24906403167b4776606944
151250179 <151250179@smail.nju.edu.cn>
2017-06-08 11:31:09 +0800

    一些实现

--- ProQuant/ProQuant/src/main/java/bl/helper/strategy/MomentumDrivenStrategy.java
+++ ProQuant/ProQuant/src/main/java/bl/helper/strategy/MomentumDrivenStrategy.java
@@ -1,9 +1,12 @@
 package bl.helper.strategy;
 import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import bl.helper.strategy.Strategy;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import PO.StockData;
 import VO.StockPlateVO;
 import VO.strategyPageVO.EarningsCircleVO;
 import VO.strategyPageVO.EarningsLineVO;
@@ -11,8 +14,11 @@ import VO.strategyPageVO.ParamDataVO;
 import VO.strategyPageVO.StrategyCallbackVO;
 import VO.strategyPageVO.StrategyEvalutingVO;
 import dataservice.BenchDataService;
+import dataservice.StockDataService;
+import dataservice.StockStrategyService;
 import model.StockPlate;
 import model.StrategyType;
+import utility.DateHelper;
 /**
  * 
  * @author yk
@@ -30,12 +36,14 @@ public class MomentumDrivenStrategy extends Strategy {
  */
 	@Autowired
 	BenchDataService benchService;
-
+    @Autowired
+    StockDataService stockDataService;
+    @Autowired
+    StockStrategyService stockStrategyService;
 	public MomentumDrivenStrategy(StockPlateVO stockPlateVO,
 			StockPlate stockPlate
 			, int possessingDays,int holdDays,int maxHoldNum, Date startDate, Date endDate, 
 			StrategyType type) {
-		//删除了holdDays参数，因为和父类的构造函数不匹配
 		super(stockPlateVO, stockPlate, possessingDays, holdDays,maxHoldNum, startDate, endDate, type);
 	}
 
@@ -44,28 +52,48 @@ public class MomentumDrivenStrategy extends Strategy {
 		
 		DatesAndBase db=StrategyHelper.stadardEarning(startDate, endDate, holdDays, stockPlate,benchService);
 		Date dates[]=db.getDates();
+		Map<Date, Double> strategyProfit=new LinkedHashMap<>();
 		int size=dates.length;
 		int count=0;
 		while(count<size){
-			int temp=count;
+			int temp=count;//开始的日期
 			count+=holdDays;
 			if(count>=size){
 				count=size-1;
 			}
+			
 			//有了开始日期和结束日期
+			Map<String , StockData> todayPO=stockStrategyService.getSomeStocks(dates[temp], stockPlateVO);
+			Map<String , StockData> lastHoldPO=null;
+			if(temp==0){
+			    lastHoldPO=stockStrategyService.getSomeStocks(DateHelper.add(startDate, -holdDays), stockPlateVO);
+		    }else{
+		    	lastHoldPO=stockStrategyService.getSomeStocks(dates[temp-holdDays], stockPlateVO);
+		    }
+			StockPlateVO resultList=StrategyHelper.getMomentumStocks(todayPO, lastHoldPO, maxHoldNum);
 			//计算可选股票
 			//筛选
+			Map<String, StockData> todResult=stockStrategyService.getSomeStocks(dates[count], resultList);
+			Map<String , StockData> yesResult=stockStrategyService.getSomeStocks(dates[temp], resultList);
 			//over
+			double profit=0.0;
+			for(Map.Entry<String , StockData> entry :todResult.entrySet()){
+				double yes=yesResult.get(entry.getKey()).getClose();
+				double tod=entry.getValue().getClose();
+				profit+=(tod-yes)/yes;
+			}
+			strategyProfit.put(dates[count], profit);
 		}
 		
+		EarningsLineVO earningsLineVO=new EarningsLineVO();
+		earningsLineVO.setBaseEarningsData(db.getBaseearning());
+		earningsLineVO.setStrategyEarningsData(strategyProfit);
 		
 		
 		
 		
 		EarningsCircleVO earningCircleVO=new EarningsCircleVO();
-		EarningsLineVO earningsLineVO=new EarningsLineVO();
 		
-		earningsLineVO.setBaseEarningsData(db.getBaseearning());
 		StrategyEvalutingVO strategyEvalutingVO=new StrategyEvalutingVO();
 		ParamDataVO paramStrategyDataVO=null;
 		ParamDataVO paramBaseDataVO=null;
@@ -73,5 +101,8 @@ public class MomentumDrivenStrategy extends Strategy {
 				strategyEvalutingVO, paramStrategyDataVO, paramBaseDataVO);
 		return strategyCallbackVO;
 	}
-
+    
+	public void calEarningCircle(EarningsCircleVO earningCircleVO,EarningsLineVO earningsLineVO){
+		
+	}
 }
